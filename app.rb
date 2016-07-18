a776ade23053f8dc8c44a9bc3dcf92e979b8d3a8
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2016-07-18 20:45:34 -0300

    Fix: I18n.load_paths fix

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ module NiftyServicesSamples
 
     configure do
       I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
-      I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
+      I18n.load_path += Dir[File.join(settings.root, 'locales', '*.yml')]
       I18n.backend.load_translations
     end
 
